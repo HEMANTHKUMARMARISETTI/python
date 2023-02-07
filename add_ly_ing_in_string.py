371515f5fbbda6bfb61bf10d388b2c8491f1722d
HEMANTHKUMARMARISETTI <124025723+HEMANTHKUMARMARISETTI@users.noreply.github.com>
2023-02-07 10:32:00 +0530

    Add files via upload

---
+++ add_ly_ing_in_string.py
@@ -0,0 +1,13 @@
+def add_string(str1):  
+  length = len(str1)  
+  
+  if length > 2:  
+    if str1[-3:] == 'ing':  
+      str1 += 'ly'  
+    else:  
+      str1 += 'ing'  
+  
+  return str1  
+print(add_string('sleep'))  
+print(add_string('amazing'))  
+print(add_string('is'))
